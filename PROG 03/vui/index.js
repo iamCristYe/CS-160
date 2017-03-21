39ccea78a947d1864fdec5219c7b0891316b5227
Ethan Rucker <Ethan Rucker>
2017-03-20 22:16:08 -0700

    combined api and alexa lambda

--- PROG 03/vui/index.js
+++ PROG 03/vui/index.js
@@ -1,178 +1,244 @@
 'use strict';
 
-var CARD_TITLE = "Recipe Assistant";
+console.log('Loading function');
 
-exports.handler = function (event, context) {
-    if (event.session.new) {
-        onSessionStarted({requestId: event.request.requestId}, event.session);
-    }
+const doc = require('dynamodb-doc');
 
-    if (event.request.type === "LaunchRequest") {
-        onLaunch(event.request,
-            event.session,
-            function callback(sessionAttributes, speechletResponse) {
-                context.succeed(buildResponse(sessionAttributes, speechletResponse));
-            });
-    } else if (event.request.type === "IntentRequest") {
-        onIntent(event.request,
-            event.session,
-            function callback(sessionAttributes, speechletResponse) {
-                context.succeed(buildResponse(sessionAttributes, speechletResponse));
-            });
-    } else if (event.request.type === "SessionEndedRequest") {
-        onSessionEnded(event.request, event.session);
-        context.succeed();
-    }
+const dynamo = new doc.DynamoDB();
+
+var Alexa = require('alexa-sdk');
+
+var APP_ID = "";
+
+var states = {
+    MAIN: '_MAIN'
+    INGREDIENTS: '_INGREDIENTS'
+    DIRECTIONS: '_DIRECTIONS'
 };
 
-/**
- * Called when the session starts.
- */
-function onSessionStarted(sessionStartedRequest, session) {
-    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId
-        + ", sessionId=" + session.sessionId);
-
-    // add any session init logic here
-}
 
 /**
- * Called when the user invokes the skill without specifying what they want.
+ * Demonstrates a simple HTTP endpoint using API Gateway. You have full
+ * access to the request and response payload, including headers and
+ * status code.
+ *
+ * To scan a DynamoDB table, make a GET request with the TableName as a
+ * query string parameter. To put, update, or delete an item, make a POST,
+ * PUT, or DELETE request respectively, passing in the payload to the
+ * DynamoDB API as a JSON body.
  */
-function onLaunch(launchRequest, session, callback) {
-    console.log("onLaunch requestId=" + launchRequest.requestId
-        + ", sessionId=" + session.sessionId);
+exports.handler = (event, context, callback) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
 
-    getWelcomeResponse(callback);
-}
+    if (event.httpMethod) {
+        const done = (err, res) => callback(null, {
+            statusCode: err ? '400' : '200',
+            body: err ? err.message : JSON.stringify(res),
+            headers: {
+                'Content-Type': 'application/json', 'Access-Control-Allow-Headers': 'x-requested-with',
+                "Access-Control-Allow-Origin" : "*", "Access-Control-Allow-Credentials" : true,
+            },
+        });
 
-/**
- * Called when the user specifies an intent for this skill.
- */
-function onIntent(intentRequest, session, callback) {
-    console.log("onIntent requestId=" + intentRequest.requestId
-        + ", sessionId=" + session.sessionId);
-
-    var intent = intentRequest.intent,
-        intentName = intentRequest.intent.name;
-
-    // dispatch custom intents to handlers here
-    if ("SearchIntent" === intentName) {
-    	handleSearchRequest(intent, session, callback);
-    } else if ("HelpIntent" === intentName) {
-    	handleHelpRequest(intent, session, callback); //Other intents ot yet completed
-    } else if ("PrevIntent" === intentName) {
-    } else if ("NextIntent" === intentName) {
-    } else if ("IngredientsIntent" === intentName) {
-    } else if ("DirectionsIntent" === intentName) {
-    } else if ("StartIntent" === intentName) {
-    } else if ("AMAZON.StartOverIntent" === intentName) {
-    } else if ("AMAZON.YesIntent" === intentName) {
-    } else if ("AMAZON.NoIntent" === intentName) {
-    } else if ("AMAZON.RepeatIntent" === intentName) {
-    } else if ("AMAZON.StopIntent" === intentName) {
+        switch (event.httpMethod) {
+            case 'DELETE':
+                dynamo.deleteItem(JSON.parse(event.body), done);
+                break;
+            case 'GET':
+                dynamo.scan({ TableName: event.queryStringParameters.TableName }, done);
+                break;
+            case 'POST':
+                dynamo.putItem(JSON.parse(event.body), done);
+                break;
+            case 'PUT':
+                dynamo.updateItem(JSON.parse(event.body), done);
+                break;
+            default:
+                done(new Error(`Unsupported method "${event.httpMethod}"`));
+        }
     } else {
-        throw "Invalid intent";
+        var alexa = Alexa.handler(event, context);
+        alexa.APP_ID = APP_ID;
+        alexa.registerHandlers(mainHandlers, ingredientsHandler, directionsHandler);
+        alexa.execute();
     }
-}
+    
+};
 
-function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
-    return {
-        outputSpeech: {
-            type: "PlainText",
-            text: output
-        },
-        card: {
-            type: "Simple",
-            title: title,
-            content: output
-        },
-        reprompt: {
-            outputSpeech: {
-                type: "PlainText",
-                text: repromptText
+var mainHandlers = Alexa.CreateStateHandler(states.MAIN, {
+    'LaunchRequest': function () {
+        this.handler.state = states.MAIN;
+        dynamo.scan({
+                TableName : "RecipesList"
+            }, function(err, data) {
+                if (err) {
+                    console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
+                } else {
+                    // print all the movies
+                    console.log("Scan succeeded.");
+                    this.attributes.tableData = data.Items;
+                }
+            });
+        this.attributes.speechOutput = "Recipe assistant, what recipe would you like to make?";
+        this.emit(':ask', this.attributes.speechOutput);
+    },
+    'SearchIntent': function () {
+        var param = intent.slots.Recipe.value;
+        Boolean recipeExists = false;
+        for (var i in this.attributes.tableData) {
+            var name = Items[i]['RecipeName'].toLowerCase();
+            if (name === param) {
+                this.attributes.ingredients = Items[i]['IngredientsList'].split("\\");
+                this.attributes.directions = Items[i]['PrepDirections'].split("\\");
+                this.attributes.speechOutput = "Found one recipe for " + name + ". Say 'what are the ingredients' for the list of ingredients or 'read recipe' to start the directions.";
+                recipeExists = true;
             }
-        },
-        shouldEndSession: shouldEndSession
-    };
-}
+        }
+        if (recipeExists === false) {
+            this.speechOutput = "Could not find a recipe by that name.";
+        }
+        this.emit(':ask', this.attributes.speechOutput);
 
-/**
- * Called when the user ends the session.
- * Is not called when the skill returns shouldEndSession=true.
- */
-function onSessionEnded(sessionEndedRequest, session) {
-    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
-        + ", sessionId=" + session.sessionId);
+    },
+    'IngredientsIntent': function () {
+        if (this.attributes.ingredients == null) {
+            this.emit(':ask', "Please pick a recipe.");
+        } else {
+            this.handler.state = states.INGREDIENTS;
+            this.emit("IngredientsIntent");
+        }
+    },
+    'DirectionsIntent': function () {
+        if (this.attributes.directions == null) {
+            this.emit(':ask', "Please pick a recipe.");
+        } else {
+            this.handler.state = states.DIRECTIONS;
+            this.emit("DirectionsIntent");
+        }
+    },
+    'AMAZON.HelpIntent': function () {
+        this.attributes.speechOutput = "You can search for a recipe by saying, 'Find,' then your recipe name.\
+        For example, 'Find pork chops'. Similar commands include: How do I cook coconut milk corned beef and cabbage?\
+        How do I make homemade cookies? I would like to prepare carrot cake oatmeal. Search for Shepherd's Pie.\
+        You can also use the commands 'exit' and 'quit' to leave the app.";
+        this.emit(':ask', this.attributes.speechOutput);
+    },
+    'AMAZON.RepeatIntent': function () {
+        this.emit(':ask', this.attributes.speechOutput);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', "Goodbye!");
+    },
+    'SessionEndedRequest': function () {
+        this.emit('AMAZON.StopIntent');
+    },
+    'Unhandled': function () {
+        this.attributes.speechOutput = "I'm sorry, I can't help you with that.";
+        this.emit(':ask', this.attributes.speechOutput);
+    },
+});
 
-    // Add any cleanup logic here
-}
+var ingredientsHandler = Alexa.CreateStateHandler(states.INGREDIENTS, {
+    'PrevIntent': function () {
+        try {
+            this.attributes.index--;
+            this.attributes.speechOutput = this.attributes.ingredients[this.attributes.index];
+            this.emit(':ask', this.attributes.speechOutput);
+        } catch {
+            this.emit(':ask', this.attributes.speechOutput);
+        }
+    },
+    'NextIntent': function () {
+        try {
+            this.attributes.index++;
+            this.attributes.speechOutput = this.attributes.ingredients[this.attributes.index];
+            this.emit(':ask', this.attributes.speechOutput);
+        } catch {
+            this.emit("DirectionsIntent");
+        }
+    },
+    'IngredientsIntent': function () {
+        this.attributes.index = 0;
+        this.attributes.speechOutput = "First ingredient: " + this.attributes.ingredients[this.attributes.index];
+        this.emit(':ask', this.attributes.speechOutput);
+    },
+    'DirectionsIntent': function () {
+        this.handler.state = states.DIRECTIONS;
+        this.emit("DirectionsIntent");
+    },
+    'AMAZON.HelpIntent': function () {
+        this.attributes.speechOutput = "You can navigate the ingredients list by saying: \
+            Previous ingredient. Next ingredient. Start again. Main menu. Exit. Quit.";
+        this.emit(':ask', this.attributes.speechOutput);
+    },
+    'AMAZON.StartOverIntent': function () {
+        this.emit("IngredientsIntent");
+    },
+    'AMAZON.RepeatIntent': function () {
+        this.emit(':ask', this.attributes.speechOutput);
+    },
+    'AMAZON.StopIntent': function () {
+        this.handler.state = states.MAIN;
+        this.emit("LaunchRequest");
+    },
+    'SessionEndedRequest': function () {
+        this.emit('AMAZON.StopIntent');
+    },
+    'Unhandled': function () {
+        this.attributes.speechOutput = "I'm sorry, I can't help you with that.";
+        this.emit(':ask', this.attributes.speechOutput);
+    },
+});
 
-function handleFinishSessionRequest(intent, session, callback) {
-    // End the session with a "Good bye!" if the user wants to quit the app
-    callback(session.attributes,
-        buildSpeechletResponseWithoutCard("Good bye!", "", true));
-}
-
-function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
-    return {
-        outputSpeech: {
-            type: "PlainText",
-            text: output
-        },
-        reprompt: {
-            outputSpeech: {
-                type: "PlainText",
-                text: repromptText
-            }
-        },
-        shouldEndSession: shouldEndSession
-    };
-}
-
-function buildResponse(sessionAttributes, speechletResponse) {
-    return {
-        version: "1.0",
-        sessionAttributes: sessionAttributes,
-        response: speechletResponse
-    };
-}
-
-function getWelcomeResponse(callback) {
-    console.log("Welcomed");
-    var sessionAttributes = {},
-        speechOutput = "Recipe assistant, what recipe would you like to make?";
-    sessionAttributes = {
-        "speechOutput": speechOutput,
-        "repromptText": speechOutput,
-        "mainDialog": true,
-        "recipeDialog": false
-    };
-    callback(sessionAttributes,
-        buildSpeechletResponse(CARD_TITLE, speechOutput, speechOutput, false));
-}
-
-//All of the intents are handled below
-
-function handleSearchRequest(intent, session, callback) {
-    console.log("Search Request");
-    /*
-	NOT COMPLETED
-	*/
-    callback(sessionAttributes,
-        buildSpeechletResponse(CARD_TITLE, speechOutput, repromptText, false));
-}
-
-function handleHelpRequest(intent, session, callback) {
-	var speechOutput = "You can search for a recipe by saying <break time='500ms'/> 'Find' then your recipe name. For example, <break time='500ms'/> 'Find pork chops'.\
-	Similar commands include: <break time='500ms'/> How do I cook coconut milk corned beef and cabbage? <break time='500ms'/> How do I make\
-	homemade cookies? <break time='500ms'/> I would like to prepare carrot cake oatmeal. <break time='500ms'/> Search for\
-	Shepherd's Pie. You can also use the commands <break time='500ms'/> 'exit' an <break time='500ms'/> 'quit' to leave the app."
-	sessionAttributes = {
-		"speechOutput": speechOutput,
-        "repromptText": speechOutput,
-        "mainDialog": session.attributes.mainDialog,
-        "recipeDialog": session.attributes.recipeDialog
-	};
-	callback(sessionAttributes,
-        buildSpeechletResponse(CARD_TITLE, speechOutput, speechOutput, false));
-}
+var directionsHandler = Alexa.CreateStateHandler(states.DIRECTIONS, {
+    'PrevIntent': function () {
+        try {
+            this.attributes.index--;
+            this.attributes.speechOutput = this.attributes.directions[this.attributes.index];
+            this.emit(':ask', this.attributes.speechOutput);
+        } catch {
+            this.emit(':ask', this.attributes.speechOutput);
+        }
+    },
+    'NextIntent': function () {
+        try {
+           this.attributes.index++;
+            this.attributes.speechOutput = this.attributes.directions[this.attributes.index];
+            this.emit(':ask', this.attributes.speechOutput); 
+        } catch {
+            this.emit('AMAZON.StopIntent');
+        }
+        
+    },
+    'IngredientsIntent': function () {
+        this.handler.state = states.INGREDIENTS;
+        this.emitWithState('IngredientsIntent');
+    },
+    'DirectionsIntent': function () {
+        this.attributes.index = 0;
+        this.attributes.speechOutput = "First Step: " + this.attributes.directions[this.attributes.index];
+        this.emit(':ask', this.attributes.speechOutput);
+    },
+    'AMAZON.HelpIntent': function () {
+        this.attributes.speechOutput = "You can navigate the preparation directions by saying: \
+            Previous step. Next step. Start again. Main menu. Exit. Quit.";
+        this.emit(':ask', this.attributes.speechOutput);
+    },
+    'AMAZON.StartOverIntent': function () {
+        this.emit('DirectionsIntent');
+    },
+    'AMAZON.RepeatIntent': function () {
+        this.emit(':ask', this.attributes.speechOutput);
+    },
+    'AMAZON.StopIntent': function () {
+        this.handler.state = states.MAIN;
+        this.emitWithState('LaunchRequest');
+    },
+    'SessionEndedRequest': function () {
+        this.emit('AMAZON.StopIntent');
+    },
+    'Unhandled': function () {
+        this.attributes.speechOutput = "I'm sorry, I can't help you with that.";
+        this.emit(':ask', this.attributes.speechOutput);
+    },
+});
